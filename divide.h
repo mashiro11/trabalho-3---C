01574694055e9e9268cc16cbe0c14b0371364a73
anacarolinalj <carolinalj94@gmail.com>
2016-05-16 12:46:36 -0300

    Update divide.h

--- divide.h
+++ divide.h
@@ -4,13 +4,22 @@
 #include "aux.h"
 #include "subtrai.h"
 
-// funcoes
+/* Funcao recebe os dois operandos da divisao e o ponteiro para a pilha.
+*
+*  Funcao determina o sinal dos operandos, chama a funcao de divisao (usando os operandos sem sinal), adiciona o sinal ao
+resultado caso seja necessario e então armazena o resultado na pilha.
+*/
 void divide_controle(char operando1[], char operando2[], no** ppmaq_pilha);
+
+/* Recebe o dividendo, divisor e o valor, onde o resultado da divisao sera armazenado.
+*
+* Enquanto o dividendo for menor que o divisor,
+*/
 void divide(char dividendo[], char divisor[], char valor[]);
 int determina_digito_quociente(char dividendo[], char divisor[], char valor[]);
 void pega_prox_num_dividir(char num_a_dividir[], char parte_num_a_dividir[], int dig, char divisor[], char valor[]);
 int analisa_maior_string(char string1[], char string2[]);
-// inclui funcao contida no divide.c 
+// inclui funcao contida no divide.c
 #include "divide.c"
 
 #endif
